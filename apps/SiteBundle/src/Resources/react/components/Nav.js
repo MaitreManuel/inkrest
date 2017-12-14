a79960c120fd081f9cefa94dbe36afcd97d940b5
Trystan Eveno <trystan.eveno@lephare.com>
2017-12-14 21:49:15 +0100

    add some navbar entries

--- apps/SiteBundle/src/Resources/react/components/Nav.js
+++ apps/SiteBundle/src/Resources/react/components/Nav.js
@@ -16,21 +16,26 @@ class Nav extends React.Component {
             <span></span>
             <span></span>
           </button>
-          <a id="home" className="navbar-brand m-auto mr-lg-4" href="javascript:void(0)">inkrest</a>
+          <NavLink to="/" id="home" className="navbar-brand m-auto mr-lg-4">
+              inkrest
+          </NavLink>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-              <li id="history" className="nav-item active">
-                <NavLink to="/products">
-                  <div>
-                    <span>Products</span>
-                  </div>
+              <li id="history" className="nav-item">
+                <NavLink to="/products" className="nav-link fadein">
+                  Products
                 </NavLink>
               </li>
-              <li id="gallery" className="nav-item">
-                <NavLink to="/account">
-                  <button className="btn btn-white fadein">
-                    <span>Log in / Sign up</span>
-                  </button>
+            </ul>
+            <ul className="navbar-nav ml-lg-auto">
+              <li className="nav-item">
+                <NavLink to="/basket" className="nav-link fadein">
+                  <i className="icon-basket icons"></i>
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to="/account" className="btn btn-white fadein">
+                  Log in / Sign up
                 </NavLink>
               </li>
             </ul>
