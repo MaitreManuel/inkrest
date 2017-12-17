295e86eba675c574146ee54e9c7d9052d11e005a
Trystan Eveno <trystan.eveno@lephare.com>
2017-12-17 20:49:30 +0100

    add polyfill clip-path

--- apps/SiteBundle/src/Resources/assets/app.js
+++ apps/SiteBundle/src/Resources/assets/app.js
@@ -1,2 +1,9 @@
+import Clipath from 'clip-path';
+
 import './main.scss';
 import './js/waves.js';
+
+ClipPath('.ring-before', '50% 0 0 0');
+ClipPath('.ring-bigger-before', '50% 0 0 0');
+ClipPath('.ring-after', '0 0 50% 0');
+ClipPath('.ring-bigger-after', '0 0 50% 0');
